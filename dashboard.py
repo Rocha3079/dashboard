64f6d77cc162d2e348cd2124aa611820e19ff8a7
Rocha3079 <rochanicolas1407@gmail.com>
2025-02-21 08:53:54 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,3 +5,11 @@ import plotly.express as px
 
 st.title("Share das Categorias")
 st.write("Filial de Bento Gonçalves")
+
+st.markdown("Tabela de Dados")
+try:
+    df = pd.read_excel(r"C:\Users\lucas.fachi\Desktop\NICOLAS - GC\PYTHON\base_teste.xlsx")
+    st.table(df)
+except:
+    st.write("Erro ao carregar a base de dados")
+    st.stop()
